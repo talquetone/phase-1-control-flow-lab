7acd1ba9830b9aefa011669cea28ec59812f22aa
Tal Quetone <tal.quetone@gmail.com>
2021-11-22 16:46:46 -0800

    commit tree 94e7fa2ce818522d1e7ef69a5d7042ab01ba6a08

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let greetingMessage
+  if (someValue <= 400){
+    greetingMessage = 'This one is on me!'
+  } 
+  else if (someValue >= 2000){
+      greetingMessage = 'I will gladly take your thirty bucks.'
+  } 
+     if (someValue >= 2500){
+        greetingMessage = 'No can do.'
+  }
+  return greetingMessage
 }
+greetingMessage; 
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(aCity){
+  let myAnswer = aCity = NYC ? "Ok, sounds good." : "No go.";
 }
+myAnswer;
 
 function switchOnCharmFromTip(){
   // Write your code here!
